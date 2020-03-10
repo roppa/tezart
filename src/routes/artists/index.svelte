e9fec5c2a142306a4354b80d1adf437e17dc0845
Mark Robson <mark.o.robson@gmail.com>
2020-03-10 16:40:57 +0000

    added cors and ipfs-mini

--- src/routes/artists/index.svelte
+++ src/routes/artists/index.svelte
@@ -1,4 +1,8 @@
 <script>
+  import IPFS from "ipfs-mini";
+
+  const ipfs = new IPFS({ host: "localhost", port: 5002, protocol: "http" });
+
   export let tez;
   let pkey = "";
   let name = "";
@@ -7,18 +11,24 @@
   let url = "";
   let image;
 
-  let realtime = true;
-  let quality = 0.1;
+  let artistIpfsHash = "";
 
   function submitProfile(event) {
     event.preventDefault();
-    console.log({
-      name,
-      bio,
-      birthDate,
-      url,
-      image
-    });
+    ipfs.addJSON(
+      {
+        name,
+        bio,
+        birthDate,
+        url,
+        image
+      },
+      (error, hash) => {
+        if (!error) {
+          artistIpfsHash = hash;
+        }
+      }
+    );
   }
 
   function previewFile(event) {
@@ -49,37 +59,45 @@
   <header>
     <h1>Artist Profile</h1>
   </header>
-  <p>
-    First you need to have a blockchain private key. If you do not have one, you
-    can generate one.
-  </p>
-  <form>
-    <label>
-      Private key
-      <input type="text" bind:value={pkey} />
-    </label>
-    <label>
-      Name
-      <input type="text" bind:value={name} required />
-    </label>
-
+  {#if !artistIpfsHash}
     <p>
-      <input on:change={previewFile} type="file" />
-      <img src={image} />
+      First you need to have a blockchain private key. If you do not have one,
+      you can generate one.
     </p>
+    <form>
+      <label>
+        Private key
+        <input type="text" bind:value={pkey} />
+      </label>
+      <label>
+        Name
+        <input type="text" bind:value={name} required />
+      </label>
 
-    <label>
-      Bio
-      <textarea cols="30" rows="10" bind:value={bio} required />
-    </label>
-    <label>
-      Date of birth
-      <input type="date" bind:value={birthDate} required />
-    </label>
-    <label>
-      Website
-      <input type="text" bind:value={url} />
-    </label>
-    <button on:click={submitProfile}>Create profile</button>
-  </form>
+      <p>
+        <input on:change={previewFile} type="file" />
+        <img src={image} />
+      </p>
+
+      <label>
+        Bio
+        <textarea cols="30" rows="10" bind:value={bio} required />
+      </label>
+      <label>
+        Date of birth
+        <input type="date" bind:value={birthDate} required />
+      </label>
+      <label>
+        Website
+        <input type="text" bind:value={url} />
+      </label>
+      <button on:click={submitProfile}>Create profile</button>
+    </form>
+  {:else}
+    <h2>Success!</h2>
+    <p>
+      Your profile address (raw) is {artistIpfsHash}. Your profile will now be
+      stored in the blockchain.
+    </p>
+  {/if}
 </section>
